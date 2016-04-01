7f40d4825dbe752886568bbf1f966d5168e91dca
dachibox <dachi.gogotchuri@gmail.com>
2016-04-01 12:07:39 +0200

    Add GeekyTool.Core implementation

---
+++ GeekyTool.Core/Base/ViewModelBase.cs
@@ -0,0 +1,32 @@
+﻿using System.Threading.Tasks;
+
+namespace GeekyTool.Core.Base
+{
+    public abstract class BaseViewModel : BindableBase
+    {
+        private bool isBusy;
+
+        public bool IsBusy
+        {
+            get { return isBusy; }
+            set
+            {
+                isBusy = value;
+                OnPropertyChanged();
+            }
+        }
+    }
+
+    //public abstract class NavigableViewModel : BaseViewModel, INavigable
+    //{
+    //    public Task OnNavigatedFrom(object e)
+    //    {
+    //        return Task.FromResult(true);
+    //    }
+
+    //    public Task OnNavigatedTo(object e)
+    //    {
+    //        return Task.FromResult(true);
+    //    }
+    //}
+}
---
+++ GeekyTool.Core/Extensions/IEnumerablesExtensions.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using GeekyTool.Core.Helpers;
+
+namespace GeekyTool.Core.Extensions
+{
+    public static class IEnumerablesExtensions
+    {
+        public static OptimizedObservableCollection<T> ToOptimizedObservableCollection<T>(this IEnumerable<T> source)
+        {
+            var result = new OptimizedObservableCollection<T>();
+            foreach (var item in source)
+                result.Add(item);
+            return result;
+        }
+
+        public static OptimizedObservableCollection<T> ToOptimizedObservableCollection<T>(this IList<T> source)
+        {
+            var result = new OptimizedObservableCollection<T>();
+            foreach (var item in source)
+                result.Add(item);
+            return result;
+        }
+
+        public static bool IsNullOrEmpty<T>(this IEnumerable<T> genericEnumerable)
+        {
+            return (genericEnumerable == null) || !genericEnumerable.Any();
+        }
+
+        public static bool IsNullOrEmpty<T>(this ICollection<T> genericCollection)
+        {
+            if (genericCollection == null)
+            {
+                return true;
+            }
+            return genericCollection.Count < 1;
+        }
+    }
+}
