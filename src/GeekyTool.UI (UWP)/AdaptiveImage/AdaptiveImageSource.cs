404ce515c1fdd4a660ea643ae8537279c1df57d3
Gabriel Ferreiro <gbril9119@gmail.com>
2016-04-17 22:56:52 +0200

    Added AdaptiveImage control, a data conservative image control.

---
+++ src/GeekyTool.UI (UWP)/AdaptiveImage/AdaptiveImageSource.cs
@@ -0,0 +1,15 @@
+﻿namespace GeekyTool
+{
+    public class AdaptiveImageSource
+    {
+        public AdaptiveImageSource(string hiResSource, string lowResSource)
+        {
+            HiResSource = hiResSource;
+            LowResSource = lowResSource;
+        }
+
+        public string HiResSource { get; private set; }
+
+        public string LowResSource { get; private set; }
+    }
+}
