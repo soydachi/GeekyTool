b9dc918515f6d5ec1c6b1d1cc4f6a5912d72c3d8
dachibox <dachi.gogotchuri@gmail.com>
2016-06-20 09:39:13 +0200

    Add BooleanToAndNegationConverter

---
+++ src/GeekyTool (UWP)/Converters/BooleanToAndNegationConverter.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Data;
+
+namespace GeekyTool.Converters
+{
+    public class BooleanToAndNegationConverter : DependencyObject, IValueConverter
+    {
+        public static readonly DependencyProperty BindableBooleanProperty = DependencyProperty.Register(
+            "BindableBoolean", typeof(bool), typeof(BooleanToAndNegationConverter), new PropertyMetadata(default(bool)));
+
+        public bool BindableBoolean
+        {
+            get { return (bool) GetValue(BindableBooleanProperty); }
+            set { SetValue(BindableBooleanProperty, value); }
+        }
+
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            var boolValue = (bool)value;
+
+            if (!boolValue & !BindableBoolean)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            var boolValue = (bool)value;
+
+            if (!boolValue & !BindableBoolean)
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
