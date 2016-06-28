d897a7cbd4fa431a0bf9e69e1bd9cff2b0a3f6d3
Gabriel Ferreiro <gbril9119@gmail.com>
2016-06-28 12:39:02 +0200

    Added CancellationTokenSource and CancellationToken extension methods

---
+++ src/GeekyTool.Core/Extensions/CancellationTokenSourceExtensions.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+
+namespace GeekyTool.Extensions
+{
+    public static class CancellationTokenSourceExtensions
+    {
+        public static void ResetIfCancelled(this CancellationTokenSource cts)
+        {
+            if (cts.IsCancellationRequested)
+                cts = new CancellationTokenSource();
+        }
+
+        public static CancellationTokenSource JoinWith(this CancellationToken ct, CancellationToken otherToken)
+        {
+            return CancellationTokenSource.CreateLinkedTokenSource(ct, otherToken);
+        }
+
+        public static CancellationTokenSource JoinWith(this CancellationToken ct, IList<CancellationToken> otherTokens)
+        {
+            otherTokens.Add(ct);
+            return CancellationTokenSource.CreateLinkedTokenSource(otherTokens.ToArray());
+        }
+    }
+}
